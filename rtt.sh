79dc8e055090adf0802e20991a919956a24a278b
foldedtoad <foldedtoad@gmail.com>
2021-05-03 20:12:07 -0700

    fix rtt.sh

--- rtt.sh
+++ rtt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # script to start RTT Console via JLinkExe
 #
-xterm -title 'JLinkExe' -e bash -c 'JLinkExe  -autoconnect 1 -device NRF52832_XXAA -if SWD -speed 1000' &
+xterm -title 'JLinkExe' -e bash -c 'JLinkExe  -autoconnect 1 -device NRF52840_XXAA -if SWD -speed 1000' &
 sleep 1
 xterm -title 'JLinkRTTClient' -e bash -c 'JLinkRTTClient' &
 echo "**********************************************************************"
