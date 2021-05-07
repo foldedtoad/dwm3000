e02ac75cf42ce02e6a62aa4fed738af4a80a990f
foldedtoad <foldedtoad@gmail.com>
2021-05-07 14:02:43 -0700

    fix typo in project name

--- examples/build_all.sh
+++ examples/build_all.sh
@@ -76,8 +76,8 @@ pushd .; cd ex_05c_ds_twr_init_sts_sdc      ; ./configure.sh; cd build; make -j4
 pushd .; cd ex_05d_ds_twr_resp_sts_sdc      ; ./configure.sh; cd build; make -j4; popd
 pushd .; cd ex_06a_ss_twr_initiator         ; ./configure.sh; cd build; make -j4; popd
 pushd .; cd ex_06b_ss_twr_responder         ; ./configure.sh; cd build; make -j4; popd
-pushd .; cd ex_06e_AES_ss_twr_initiator     ; ./configure.sh; cd build; make -j4; popd
-pushd .; cd ex_06f_AES_ss_twr_responder     ; ./configure.sh; cd build; make -j4; popd
+pushd .; cd ex_06e_aes_ss_twr_initiator     ; ./configure.sh; cd build; make -j4; popd
+pushd .; cd ex_06f_aes_ss_twr_responder     ; ./configure.sh; cd build; make -j4; popd
 pushd .; cd ex_07a_ack_data_tx              ; ./configure.sh; cd build; make -j4; popd
 pushd .; cd ex_07b_ack_data_rx              ; ./configure.sh; cd build; make -j4; popd
 pushd .; cd ex_07c_ack_data_rx_dbl_buff     ; ./configure.sh; cd build; make -j4; popd
@@ -114,8 +114,8 @@ cp ./ex_05c_ds_twr_init_sts_sdc/build/zephyr/zephyr.hex      ./bin/ex_05c_ds_twr
 cp ./ex_05d_ds_twr_resp_sts_sdc/build/zephyr/zephyr.hex      ./bin/ex_05d_ds_twr_resp_sts_sdc.hex
 cp ./ex_06a_ss_twr_initiator/build/zephyr/zephyr.hex         ./bin/ex_06a_ss_twr_initiator.hex
 cp ./ex_06b_ss_twr_responder/build/zephyr/zephyr.hex         ./bin/ex_06b_ss_twr_responder.hex
-cp ./ex_06e_AES_ss_twr_initiator/build/zephyr/zephyr.hex     ./bin/ex_06e_AES_ss_twr_initiator.hex
-cp ./ex_06f_AES_ss_twr_responder/build/zephyr/zephyr.hex     ./bin/ex_06f_AES_ss_twr_responder.hex
+cp ./ex_06e_aes_ss_twr_initiator/build/zephyr/zephyr.hex     ./bin/ex_06e_AES_ss_twr_initiator.hex
+cp ./ex_06f_aes_ss_twr_responder/build/zephyr/zephyr.hex     ./bin/ex_06f_AES_ss_twr_responder.hex
 cp ./ex_07a_ack_data_tx/build/zephyr/zephyr.hex              ./bin/ex_07a_ack_data_tx.hex
 cp ./ex_07b_ack_data_rx/build/zephyr/zephyr.hex              ./bin/ex_07b_ack_data_rx.hex
 cp ./ex_07c_ack_data_rx_dbl_buff/build/zephyr/zephyr.hex     ./bin/ex_07c_ack_data_rx_dbl_buff.hex
@@ -152,8 +152,8 @@ rm -rf ex_05c_ds_twr_init_sts_sdc/build
 rm -rf ex_05d_ds_twr_resp_sts_sdc/build
 rm -rf ex_06a_ss_twr_initiator/build
 rm -rf ex_06b_ss_twr_responder/build
-rm -rf ex_06e_AES_ss_twr_initiator/build
-rm -rf ex_06f_AES_ss_twr_responder/build
+rm -rf ex_06e_aes_ss_twr_initiator/build
+rm -rf ex_06f_aes_ss_twr_responder/build
 rm -rf ex_07a_ack_data_tx/build
 rm -rf ex_07b_ack_data_rx/build
 rm -rf ex_07c_ack_data_rx_dbl_buff/build
