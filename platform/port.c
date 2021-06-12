32b379e614ac0cb6442ebdb7537f2fe44a7145ed
foldedtoad <foldedtoad@gmail.com>
2021-06-11 18:15:32 -0700

    major rework of port handling and inclusion of spi PHA & POL config

--- platform/port.c
+++ platform/port.c
@@ -42,24 +42,47 @@ LOG_MODULE_REGISTER(port);
 #define IRQ_GPIO_PIN      DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_irq_gpios, pin)
 #define IRQ_GPIO_FLAGS    DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_irq_gpios, flags)
 
-#define WAKEUP_GPIO_PIN   DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_wakeup_gpios, pin)
-#define WAKEUP_GPIO_FLAGS DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_wakeup_gpios, flags)
+#define WAKEUP_GPIO_PORT   DT_LABEL(DT_PHANDLE_BY_IDX(DT_INST(0, qorvo_dwm3000), dwm_wakeup_gpios, 0))
+#define WAKEUP_GPIO_PIN    DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_wakeup_gpios, pin)
+#define WAKEUP_GPIO_FLAGS  DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_wakeup_gpios, flags)
 
-#define RESET_GPIO_PIN    DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_reset_gpios, pin)
-#define RESET_GPIO_FLAGS  DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_reset_gpios, flags)
+#define RESET_GPIO_PORT    DT_LABEL(DT_PHANDLE_BY_IDX(DT_INST(0, qorvo_dwm3000), dwm_reset_gpios, 0))
+#define RESET_GPIO_PIN     DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_reset_gpios, pin)
+#define RESET_GPIO_FLAGS   DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_reset_gpios, flags)
 
+#define RX_LED_GPIO_PORT   DT_LABEL(DT_PHANDLE_BY_IDX(DT_INST(0, qorvo_dwm3000), dwm_rx_led_gpios, 0))
 #define RX_LED_GPIO_PIN    DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_rx_led_gpios, pin)
 #define RX_LED_GPIO_FLAGS  DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_rx_led_gpios, flags)
 
+#define TX_LED_GPIO_PORT   DT_LABEL(DT_PHANDLE_BY_IDX(DT_INST(0, qorvo_dwm3000), dwm_rx_led_gpios, 0))
 #define TX_LED_GPIO_PIN    DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_tx_led_gpios, pin)
 #define TX_LED_GPIO_FLAGS  DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_tx_led_gpios, flags)
+
+#define IRQ_GPIO_PORT      DT_LABEL(DT_PHANDLE_BY_IDX(DT_INST(0, qorvo_dwm3000), dwm_irq_gpios, 0))
+#define IRQ_GPIO_PIN       DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_irq_gpios, pin)
+#define IRQ_GPIO_FLAGS     DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_irq_gpios, flags)
+
+#define PHA_GPIO_PORT      DT_LABEL(DT_PHANDLE_BY_IDX(DT_INST(0, qorvo_dwm3000), dwm_spi_pha_gpios, 0))
+#define PHA_GPIO_PIN       DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_spi_pha_gpios, pin)
+#define PHA_GPIO_FLAGS     DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_spi_pha_gpios, flags)
+
+#define POL_GPIO_PORT      DT_LABEL(DT_PHANDLE_BY_IDX(DT_INST(0, qorvo_dwm3000), dwm_spi_pol_gpios, 0))
+#define POL_GPIO_PIN       DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_spi_pol_gpios, pin)
+#define POL_GPIO_FLAGS     DT_PHA(DT_INST(0, qorvo_dwm3000), dwm_spi_pol_gpios, flags)
 
 /****************************************************************************//**
  *
  *******************************************************************************/
 
-static const struct device * gpio_dev = NULL;
 static struct gpio_callback gpio_cb;
+
+static const struct device * wakeup_dev = NULL;
+static const struct device * reset_dev  = NULL;
+static const struct device * rx_led_dev = NULL;
+static const struct device * tx_led_dev = NULL;
+static const struct device * irq_dev    = NULL;
+static const struct device * pha_dev    = NULL;
+static const struct device * pol_dev    = NULL;
 
 /****************************************************************************//**
  *
@@ -116,35 +139,68 @@ void Sleep(uint32_t x)
  * */
 int peripherals_init (void)
 {
-    if (gpio_dev == NULL) {
-        //LOG_INF("%s: GPIO controller: \"%s\"", __func__, DWM_GPIO_NAME);
-        gpio_dev = device_get_binding(DWM_GPIO_NAME);
-        if (!gpio_dev) {
-            LOG_ERR("error: \"%s\" not found", DWM_GPIO_NAME);
-            return -1;
-        }
-    }
-
     /* Wakeup */
-    LOG_INF("Configure WAKEUP pin");
-    gpio_pin_configure(gpio_dev, WAKEUP_GPIO_PIN, GPIO_OUTPUT);
+    LOG_INF("Configure WAKEUP pin on port \"%s\" pin %d", WAKEUP_GPIO_PORT, WAKEUP_GPIO_PIN);
+    wakeup_dev = device_get_binding(WAKEUP_GPIO_PORT);
+    if (!wakeup_dev) {
+        LOG_ERR("error: \"%s\" not found", WAKEUP_GPIO_PORT);
+        return -1;
+    }
+    gpio_pin_configure(wakeup_dev, WAKEUP_GPIO_PIN, GPIO_OUTPUT);
+    gpio_pin_set(wakeup_dev, WAKEUP_GPIO_PIN, 1);
 
     /* Reset */
-    LOG_INF("Configure RESET pin");
-    gpio_pin_configure(gpio_dev, RESET_GPIO_PIN, GPIO_OUTPUT);
-    gpio_pin_set(gpio_dev, RESET_GPIO_PIN, 1);  // insure pin not held in reset
+    LOG_INF("Configure RESET pin on port \"%s\" pin %d", RESET_GPIO_PORT, RESET_GPIO_PIN);
+    reset_dev = device_get_binding(RESET_GPIO_PORT);
+    if (!reset_dev) {
+        LOG_ERR("error: \"%s\" not found", RESET_GPIO_PORT);
+        return -1;
+    }
+    gpio_pin_configure(reset_dev, RESET_GPIO_PIN, GPIO_OUTPUT);
+    gpio_pin_set(reset_dev, RESET_GPIO_PIN, 1);
 
     /* RX LED */
-    LOG_INF("Configure RX LED pin");
-    gpio_pin_configure(gpio_dev, RX_LED_GPIO_PIN, GPIO_OUTPUT);
+    LOG_INF("Configure RX LED pin on port \"%s\" pin %d", RX_LED_GPIO_PORT, RX_LED_GPIO_PIN);
+    rx_led_dev = device_get_binding(RX_LED_GPIO_PORT);
+    if (!rx_led_dev) {
+        LOG_ERR("error: \"%s\" not found", RX_LED_GPIO_PORT);
+        return -1;
+    }
+    gpio_pin_configure(rx_led_dev, RX_LED_GPIO_PIN, GPIO_OUTPUT);
+    gpio_pin_set(rx_led_dev, RX_LED_GPIO_PIN, 1);
 
     /* TX LED */
-    LOG_INF("Configure TX LED pin");
-    gpio_pin_configure(gpio_dev, TX_LED_GPIO_PIN, GPIO_OUTPUT);
+    LOG_INF("Configure TX LED pin on port \"%s\" pin %d", TX_LED_GPIO_PORT, TX_LED_GPIO_PIN);
+    tx_led_dev = device_get_binding(RX_LED_GPIO_PORT);
+    if (!tx_led_dev) {
+        LOG_ERR("error: \"%s\" not found", TX_LED_GPIO_PORT);
+        return -1;
+    }
+    gpio_pin_configure(tx_led_dev, TX_LED_GPIO_PIN, GPIO_OUTPUT);
+    gpio_pin_set(tx_led_dev, TX_LED_GPIO_PIN, 1);
+
+    /* SPI PHASE */
+    LOG_INF("Configure SPI Phase pin on port \"%s\" pin %d", PHA_GPIO_PORT, PHA_GPIO_PIN);
+    pha_dev = device_get_binding(PHA_GPIO_PORT);
+    if (!pha_dev) {
+        LOG_ERR("error: \"%s\" not found", PHA_GPIO_PORT);
+        return -1;
+    }
+    gpio_pin_configure(pha_dev, PHA_GPIO_PIN, GPIO_OUTPUT);
+    gpio_pin_set(pha_dev, PHA_GPIO_PIN, 0);
+
+    /* SPI POLARITY */
+    LOG_INF("Configure SPI Polarity pin on port \"%s\" pin %d", POL_GPIO_PORT, POL_GPIO_PIN);
+    pol_dev = device_get_binding(POL_GPIO_PORT);
+    if (!pol_dev) {
+        LOG_ERR("error: \"%s\" not found", POL_GPIO_PORT);
+        return -1;
+    }
+    gpio_pin_configure(pol_dev, POL_GPIO_PIN, GPIO_OUTPUT);
+    gpio_pin_set(pol_dev, POL_GPIO_PIN, 0);
 
     return 0;
 }
-
 
 /* @fn    spi_peripheral_init
  * */
@@ -177,20 +233,21 @@ void reset_DWIC(void)
 
 #if 0
     /*
-     * User RSTn gpio pin to reset DWM3000.
+     * Use RSTn gpio pin to reset DWM3000.
      */
-    // Enable GPIO used for DW3000 reset as open collector output
-    gpio_pin_configure(gpio_dev, RESET_GPIO_PIN, (GPIO_OUTPUT | GPIO_OPEN_DRAIN));
 
-    // Drive the RSTn pin low
-    gpio_pin_set(gpio_dev, RESET_GPIO_PIN, 0);
+    /* Enable GPIO used for DW3000 reset as open collector output */
+    gpio_pin_configure(reset_dev, RESET_GPIO_PIN, (GPIO_OUTPUT | GPIO_OPEN_DRAIN));
+
+    /* Drive the RSTn pin low */
+    gpio_pin_set(reset_dev, RESET_GPIO_PIN, 0);
 
     deca_usleep(10);
 
-    // Drive the RSTn pin high
-    gpio_pin_set(gpio_dev, RESET_GPIO_PIN, 1);
+    /* Drive the RSTn pin high */
+    gpio_pin_set(reset_dev, RESET_GPIO_PIN, 1);
 
-    // Put the pin back to output open-drain (not active)
+    /* Put the pin back to output open-drain (not active) */
     setup_DW3000RSTnIRQ(0);
 
     Sleep(2);
@@ -217,12 +274,12 @@ void reset_DWIC(void)
 void setup_DW3000RSTnIRQ(int enable)
 {
     if (enable) {
-        // Enable GPIO used as DECA RESET for interrupt
-        gpio_pin_configure(gpio_dev, RESET_GPIO_PIN, (GPIO_OUTPUT | GPIO_OPEN_DRAIN | GPIO_INT_EDGE_RISING));
+        /* Enable GPIO used as DECA RESET for interrupt */
+        gpio_pin_configure(reset_dev, RESET_GPIO_PIN, (GPIO_OUTPUT | GPIO_OPEN_DRAIN | GPIO_INT_EDGE_RISING));
     }
     else {
-        // Put the pin back to tri-state, as output open-drain (not active)
-        gpio_pin_configure(gpio_dev, RESET_GPIO_PIN, (GPIO_OUTPUT | GPIO_OPEN_DRAIN));
+        /* Put the pin back to tri-state, as output open-drain (not active) */
+        gpio_pin_configure(reset_dev, RESET_GPIO_PIN, (GPIO_OUTPUT | GPIO_OPEN_DRAIN));
     }
 }
 
@@ -238,9 +295,9 @@ void setup_DW3000RSTnIRQ(int enable)
  */
  void wakeup_device_with_io(void)
 {
-    gpio_pin_set(gpio_dev, WAKEUP_GPIO_PIN, 1);
+    gpio_pin_set(wakeup_dev, WAKEUP_GPIO_PIN, 1);
     deca_usleep(500);
-    gpio_pin_set(gpio_dev, WAKEUP_GPIO_PIN, 0);
+    gpio_pin_set(wakeup_dev, WAKEUP_GPIO_PIN, 0);
 
 }
 
@@ -258,9 +315,9 @@ void make_very_short_wakeup_io(void)
 {
     uint8_t   cnt;
 
-    gpio_pin_set(gpio_dev, WAKEUP_GPIO_PIN, 1);
+    gpio_pin_set(wakeup_dev, WAKEUP_GPIO_PIN, 1);
     for (cnt=0; cnt<10; cnt++)  __NOP();
-    gpio_pin_set(gpio_dev, WAKEUP_GPIO_PIN, 0);
+    gpio_pin_set(wakeup_dev, WAKEUP_GPIO_PIN, 0);
 }
 
 /* @fn      led_off
@@ -270,10 +327,10 @@ void led_off (led_t led)
 {
     switch (led) {
         case 0:
-            gpio_pin_set(gpio_dev, RX_LED_GPIO_PIN, 0);
+            gpio_pin_set(rx_led_dev, RX_LED_GPIO_PIN, 0);
             break;
         case 1:
-            gpio_pin_set(gpio_dev, TX_LED_GPIO_PIN, 0);
+            gpio_pin_set(tx_led_dev, TX_LED_GPIO_PIN, 0);
             break;
         default:
             // do nothing for undefined led number
@@ -288,10 +345,10 @@ void led_on (led_t led)
 {
     switch (led) {
         case 0:
-            gpio_pin_set(gpio_dev, RX_LED_GPIO_PIN, 1);
+            gpio_pin_set(rx_led_dev, RX_LED_GPIO_PIN, 1);
             break;
         case 1:
-            gpio_pin_set(gpio_dev, TX_LED_GPIO_PIN, 1);
+            gpio_pin_set(tx_led_dev, TX_LED_GPIO_PIN, 1);
             break;
         default:
             // do nothing for undefined led number
@@ -305,7 +362,7 @@ void led_on (led_t led)
  * */
 void port_wakeup_dw3000(void)
 {
-    gpio_pin_set(gpio_dev, WAKEUP_GPIO_PIN, 0);
+    gpio_pin_set(wakeup_dev, WAKEUP_GPIO_PIN, 0);
     //TODO
 }
 
@@ -425,11 +482,11 @@ uint32_t port_CheckEXT_IRQ(void)
  */
 void port_set_dwic_isr(port_deca_isr_t deca_isr)
 {
-    if (gpio_dev == NULL) {
-        LOG_INF("%s: Binding to %s and pin %d\n", __func__, DWM_GPIO_NAME, IRQ_GPIO_PIN);
-        gpio_dev = device_get_binding(DWM_GPIO_NAME);
-        if (!gpio_dev) {
-            LOG_ERR("error: \"%s\" not found", DWM_GPIO_NAME);
+    if (irq_dev == NULL) {
+        LOG_INF("Configure IRQ on port \"%s\" pin %d", IRQ_GPIO_PORT, IRQ_GPIO_PIN);
+        irq_dev = device_get_binding(IRQ_GPIO_PORT);
+        if (!irq_dev) {
+            LOG_ERR("error: \"%s\" not found", IRQ_GPIO_PORT);
             return;
         }
     }
@@ -437,13 +494,13 @@ void port_set_dwic_isr(port_deca_isr_t deca_isr)
     LOG_INF("Configure IRQ pin");
 
     /* Decawave interrupt */
-    gpio_pin_configure(gpio_dev, IRQ_GPIO_PIN, (GPIO_INPUT | IRQ_GPIO_FLAGS));
+    gpio_pin_configure(irq_dev, IRQ_GPIO_PIN, (GPIO_INPUT | IRQ_GPIO_FLAGS));
 
     gpio_init_callback(&gpio_cb, (gpio_callback_handler_t)(deca_isr), BIT(IRQ_GPIO_PIN));
 
-    gpio_add_callback(gpio_dev, &gpio_cb);
+    gpio_add_callback(irq_dev, &gpio_cb);
 
-    gpio_pin_interrupt_configure(gpio_dev, IRQ_GPIO_PIN, GPIO_INT_EDGE_RISING);
+    gpio_pin_interrupt_configure(irq_dev, IRQ_GPIO_PIN, GPIO_INT_EDGE_RISING);
 }
 
 /****************************************************************************//**
